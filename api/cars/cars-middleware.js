33ab53d798bcebc99028edd1a14b66367a7635c9
Chaz Carbis <chazcarbis@gmail.com>
2021-07-13 18:37:04 -0500

    Built out cars model

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,22 @@
-const checkCarId = (req, res, next) => {
+const Cars = require('./cars-model')
+
+async function checkCarId(req, res, next) {
+  try{ 
+    const car = await Cars.getAll
+  }
+  catch {
+
+  }
+}
+
+async function checkCarPayload(req, res, next) {
   // DO YOUR MAGIC
 }
 
-const checkCarPayload = (req, res, next) => {
+async function checkVinNumberValid(req, res, next) {
   // DO YOUR MAGIC
 }
 
-const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
-}
-
-const checkVinNumberUnique = (req, res, next) => {
+async function checkVinNumberUnique(req, res, next) {
   // DO YOUR MAGIC
 }
